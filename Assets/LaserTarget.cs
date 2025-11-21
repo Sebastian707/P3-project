4fdd94f292d932c9446e37f61acf3128d5bf0033
Vahigoku <vahigoku@yahoo.com>
2025-11-21 16:15:51 +0100

    Puzzle sequence making in the making

--- Assets/LaserTarget.cs
+++ Assets/LaserTarget.cs
@@ -4,15 +4,29 @@ public class LaserTarget : MonoBehaviour
 {
     public bool lightTouched = false;
     public int touchPoint = 0;
+    public Material originalMat;
+    public Material hitMat;
+    public Material currentMat;
+
+    public void Start()
+    {
+        originalMat = GetComponent<Renderer>().material;
+        currentMat = originalMat;
+
+    }
     public void Update()
     {
         if (lightTouched)
         {
             touchPoint = 1;
+            currentMat = hitMat;
+            GetComponent<Renderer>().material = currentMat;
         }
         else
         {
             touchPoint = 0;
+            currentMat = originalMat;
+            GetComponent<Renderer>().material = currentMat;
         }
     }
 
